e167412c5d9a8e56886145ac9a41f58161f5060d
zwjhuhu <zwjhuhu@126.com>
2015-08-18 17:13:36 +0800

    clean

--- obdandroid/obdserver/gen/com/skywin/obdserver/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.skywin.obdserver;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- obdandroid/odbbtreader/gen/com/skywin/obdreader/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.skywin.obdreader;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
